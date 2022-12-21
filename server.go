1c3cb9ffa89d3826e7d33159624446beeffe079f
sing3demons <sing3demons@live.com>
2022-12-21 20:29:55 +0700

    :sparkles: init structure

--- server.go
+++ server.go
@@ -1,11 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
+
+	_ "github.com/lib/pq"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
+func initDB() *sql.DB {
+	connStr := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createTb := `
+	CREATE TABLE IF NOT EXISTS expenses (
+		id SERIAL PRIMARY KEY,
+		title TEXT,
+		amount FLOAT,
+		note TEXT,
+		tags TEXT[]
+	);
+	`
+	_, err = db.Exec(createTb)
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
+
+	return db
+}
+
 func main() {
-	fmt.Println("Please use server.go for main file")
+	db := initDB()
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	fmt.Println("start at port:", os.Getenv("PORT"))
+	e.Start(":" + os.Getenv("PORT"))
 }
