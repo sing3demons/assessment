c80c8860b20ac418801af952a305520772af42b2
sing3demons <sing3demons@live.com>
2022-12-24 13:00:54 +0700

    :white_check_mark: TestUpdateExpensesHandler

--- handler/expenses/expenses_test.go
+++ handler/expenses/expenses_test.go
@@ -106,3 +106,42 @@ func TestGetExpensesHandlerByID(t *testing.T) {
 		assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
 	}
 }
+
+func TestUpdateExpensesHandler(t *testing.T) {
+	// Arrange
+	data := `{
+		"title": "apple smoothie",
+		"amount": 89,
+		"note": "no discount",
+		"tags": ["beverage"]
+	}`
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPut, "/expenses/:id", strings.NewReader(data))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	// expected
+	expected := "{\"id\":1,\"title\":\"apple smoothie\",\"amount\":89,\"note\":\"no discount\",\"tags\":[\"beverage\"]}"
+
+	db, mock, err := sqlmock.New()
+	stmt := mock.ExpectPrepare("UPDATE expenses SET title=\\$2, amount=\\$3, note=\\$4, tags=\\$5 WHERE id=\\$1")
+	stmt.ExpectExec().
+		WithArgs(1, "apple smoothie", 89.00, "no discount", pq.Array([]string{"beverage"})).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	h := handler{db}
+	c := e.NewContext(req, rec)
+
+	c.SetParamNames("id")
+	c.SetParamValues("1")
+	err = h.UpdateExpensesHandler(c)
+
+	// assertion
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
+	}
+}
--- handler/expenses/update.go
+++ handler/expenses/update.go
@@ -2,6 +2,7 @@ package expenses
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -9,26 +10,20 @@ import (
 func (h *handler) UpdateExpensesHandler(c echo.Context) error {
 	var m NewsExpenses
 
-	id := c.Param("id")
+	id, _ := strconv.Atoi(c.Param("id"))
 
 	if err := c.Bind(&m); err != nil {
 		return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
 	}
-
+	m.ID = id
 	stmt, err := h.DB.Prepare("UPDATE expenses SET title=$2, amount=$3, note=$4, tags=$5 WHERE id=$1")
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
 	}
-	_, err = stmt.Exec(id, m.Title, m.Amount, m.Note, m.Tags)
+	_, err = stmt.Exec(m.ID, m.Title, m.Amount, m.Note, m.Tags)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusOK, echo.Map{
-		"id":     m.ID,
-		"title":  m.Title,
-		"amount": m.Amount,
-		"note":   m.Note,
-		"tags":   m.Tags,
-	})
+	return c.JSON(http.StatusOK, m)
 }
